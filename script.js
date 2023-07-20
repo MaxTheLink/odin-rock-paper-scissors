5230d095e585cad88f169d7296b43a0e2886bc49
MaxTheLink <122754324+MaxTheLink@users.noreply.github.com>
2023-07-20 16:58:08 +0200

    fix: Cleanup & restart functionality

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const ROUND_RESULT_VICTORY = "victory";
 const ROUND_RESULT_DEFEAT = "defat";
 const ROUND_RESULT_DRAW = "draw";
 
+let playerScore = 0;
+let computerScore = 0;
+
+const gameButtons = document.querySelectorAll(".game-button");
+const restartButton = document.querySelector("#restart-button");
+gameButtons.forEach((button) => {
+  button.addEventListener("click", playRound.bind(this, button.id));
+});
+restartButton.addEventListener("click", resetGame.bind(this, restartButton.id));
+
 function getComputerChoice() {
   const options = ["Rock", "Paper", "Scissors"];
   const randomChoice = Math.floor(Math.random() * 3);
@@ -9,8 +19,8 @@ function getComputerChoice() {
   return options[randomChoice];
 }
 
-function determineWinner(playerChoice, computerChoice) {
-  switch (playerChoice.toLowerCase() + computerChoice.toLowerCase()) {
+function determineWinner(playerSelection, computerSelection) {
+  switch (playerSelection.toLowerCase() + computerSelection.toLowerCase()) {
     case "rockscissors":
     case "scissorspaper":
     case "paperrock":
@@ -30,12 +40,18 @@ function playRound(playerSelection) {
   const computerSelection = getComputerChoice();
   const roundResult = determineWinner(playerSelection, computerSelection);
 
+  const playerChoice = document.querySelector(".player-choice");
+  const computerChoice = document.querySelector(".computer-choice");
+
+  playerChoice.textContent = playerSelection;
+  computerChoice.textContent = computerSelection;
+
   let resultMessage = "";
-  if (roundResult == "draw") {
+  if (roundResult == ROUND_RESULT_DRAW) {
     resultMessage = `It's a draw! ${playerSelection} against ${computerSelection}`;
-  } else if (roundResult == "victory") {
+  } else if (roundResult == ROUND_RESULT_VICTORY) {
     resultMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
-  } else if (roundResult == "defeat") {
+  } else if (roundResult == ROUND_RESULT_DEFEAT) {
     resultMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
 
@@ -44,20 +60,29 @@ function playRound(playerSelection) {
   checkForWinners();
 }
 function checkForWinners() {
-  if (playerScore == "5") {
-    winnerText.textContent = "Game Over!\n The player won!";
-    buttons.forEach((button) => {
+  const winnerText = document.querySelector(".round-winner");
+  if (playerScore == "5" || computerScore == "5") {
+    gameButtons.forEach((button) => {
       button.disabled = true;
     });
-  } else if (computerScore == "5") {
-    winnerText.textContent = "Game Over! The computer won!";
-    buttons.forEach((button) => {
-      button.disabled = true;
-    });
+    restartButton.disabled = false;
+    if (playerScore == "5") {
+      winnerText.textContent = "Game Over!\n The player won!";
+    } else if (computerScore == "5") {
+      winnerText.textContent = "Game Over! The computer won!";
+    }
   }
 }
 
 function applyRoundResult(resultMessage, roundResult) {
+  const winnerText = document.querySelector(".round-winner");
+  const playerScoreOutput = document.querySelector(
+    ".player-score .score-value"
+  );
+  const computerScoreOutput = document.querySelector(
+    ".computer-score .score-value"
+  );
+
   winnerText.textContent = resultMessage;
   if (roundResult == ROUND_RESULT_DRAW) {
     return;
@@ -70,19 +95,19 @@ function applyRoundResult(resultMessage, roundResult) {
   }
 }
 
-const buttons = document.querySelectorAll("button");
-const winnerText = document.querySelector(".round-winner");
-const playerScoreOutput = document.querySelector(".player-score .score-value");
-const computerScoreOutput = document.querySelector(
-  ".computer-score .score-value"
-);
+function resetGame() {
+  document.querySelector(".player-choice").textContent = "Player";
+  document.querySelector(".computer-choice").textContent = "Computer";
+  document.querySelector(".player-score .score-value").textContent = "0";
+  document.querySelector(".computer-score .score-value").textContent = "0";
+  document.querySelector(".round-winner").textContent =
+    "Press a button to start the game";
 
-let playerScore = 0;
-let computerScore = 0;
+  gameButtons.forEach((button) => {
+    button.disabled = false;
+  });
+  restartButton.disabled = true;
 
-playerScoreOutput.textContent = playerScore;
-computerScoreOutput.textContent = computerScore;
-
-buttons.forEach((button) => {
-  button.addEventListener("click", playRound.bind(this, button.id));
-});
+  playerScore = 0;
+  computerScore = 0;
+}
